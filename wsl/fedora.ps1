d38cebfc6bb2f0c216b60f340b638195cd9b9a8d
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2023-12-08 06:12:45 -0500

    initial framework

---
+++ wsl/fedora.ps1
@@ -0,0 +1,5 @@
+# General updates
+wsl -d fedora38 -u root -e  dnf update -y
+wsl -d fedora38 -u root -e  dnf upgrade -y
+
+# OS version upgrade reference - https://itsfoss.com/upgrade-fedora-version/
---
+++ wsl/identify_distros.ps1
@@ -0,0 +1,2 @@
+# reference - https://www.hanselman.com/blog/keeping-your-wsl-linux-instances-up-to-date-automatically-within-windows-10
+wsl --list -v
