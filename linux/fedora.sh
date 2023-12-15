37e3d61ca4dd011c2b68f7f48b7b46a1909e8fd6
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2023-12-15 11:48:15 -0500

    added fedora update commands

---
+++ linux/fedora.sh
@@ -0,0 +1,9 @@
+
+sudo dnf update -y
+sudo dnf upgrade -y
+
+# sudo dnf upgrade --refresh -y
+# sudo dnf install dnf-plugin-system-upgrade -y
+# sudo dnf system-upgrade download --releasever=39 -y
+
+# sudo dnf system-upgrade reboot -y
