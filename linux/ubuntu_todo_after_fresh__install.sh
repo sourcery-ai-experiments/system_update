f03058f05836cd8e5bace4adb614278ac84f3563
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-03-29 18:51:10 -0400

    ubuntu setup steps - some more additions and refinement - 02

--- linux/ubuntu_todo_after_fresh__install.sh
+++ linux/ubuntu_todo_after_fresh__install.sh
@@ -204,7 +204,7 @@ sudo apt install -y openjdk-22-jdk
 # Install vscode
 # Documentation: https://code.visualstudio.com/docs/setup/linux 
 
-sudo apt-get install -y wget gpg
+sudo apt install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -335,7 +335,7 @@ flatpak run com.usebottles.bottles
 # Some network stuff
 sudo apt install -y net-tools
 sudo apt install -y nmap # https://itsfoss.com/how-to-find-what-devices-are-connected-to-network-in-ubuntu/
-sudo snap install nutty
+# sudo snap install nutty # Does not seem to open when clicked
 
 # Install any of the following as needed
 # sudo apt install -y gimp gparted cargo curl wget unrar unzip 
