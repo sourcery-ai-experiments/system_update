2c50a1262bbc35f0f424c7e5ff7ebe38b8c8dce2
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-04-21 12:13:09 -0400

    ubuntu - corrections and user management

--- linux/ubuntu/ubuntu_todo_before_system_format.sh
+++ linux/ubuntu/ubuntu_todo_before_system_format.sh
@@ -1,2 +1,9 @@
 
-# TODO
+# List of packages/apps installed
+# maybe make a cron job which does the following
+
+apt list > apt_list.txt
+apt list --installed > apt_list_installed.txt
+flatpak list > flatpak_list.txt
+snap list > snap_list.
+
