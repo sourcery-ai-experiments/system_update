341110eb3abd7b0b350756128cc28783aec6f75e
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-05-02 15:15:58 -0400

    ubuntu - get Logitech mouse working

--- linux/ubuntu/ubuntu_todo_after_fresh_install.sh
+++ linux/ubuntu/ubuntu_todo_after_fresh_install.sh
@@ -27,6 +27,18 @@ gedit system_update/linux/ubuntu/ubuntu_todo_after_fresh_install.sh
 ###########################################
 ###########################################
 
+### Logitech mouse
+sudo apt install solaar logiops
+
+# Mouse M185 worked right away after connecting the USB receiver without installing these.
+# But mouse M720 didn't work with USB receiver right away.
+# solaar seems to be the most famous software to get Logitech mice working with linux. Installed it, and it didn't detect M720.
+# So searched for what I could do and found logiops. Installed it and M720 started working. I don't remember if it worked after reboot or right away.
+# Thought of removing solaar, but now it shows various options to configure different mouse options. 
+
+###########################################
+###########################################
+
 ##### Fonts setup early to make zsh look good from beginning (needs nerd fonts)
 
 # Commonly used media codecs and fonts for Ubuntu
