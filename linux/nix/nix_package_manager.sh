7c798301f8a2bf2e658e94653bf841e474cd23be
Nikhil Bhirud <12810336+nbhirud@users.noreply.github.com>
2024-05-05 15:26:56 -0400

    nix - nix package manager installation instructions for linux systems

---
+++ linux/nix/nix_package_manager.sh
@@ -0,0 +1,13 @@
+
+# Install
+# https://nixos.org/manual/nix/stable/installation/
+# https://nixos.org/manual/nix/stable/installation/upgrading
+
+$ bash <(curl -L https://nixos.org/nix/install) --daemon
+
+
+# Usage:
+# https://itsfoss.com/ubuntu-install-nix-package-manager/
+
+
+
